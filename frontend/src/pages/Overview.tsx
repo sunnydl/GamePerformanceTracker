4e5691fc00c2a367d8ae8d992b4475efe967990e
htran25 <htran25@ucsc.edu>
2021-10-15 14:01:31 -0700

    connect overview information with the user state

--- frontend/src/pages/Overview.tsx
+++ frontend/src/pages/Overview.tsx
@@ -1,42 +1,40 @@
 import React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import { Typography, Container, Avatar } from '@mui/material';
 
 import { useAppSelector, useAppDispatch } from '../redux/hooks';
 import { setUserData } from '../redux/slices/user';
-import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-
-const WhiteTextTypography = withStyles({
-  root: {
-    color: "#FFFFFF"
-  }
-})(Typography);
 
 function Overview() {
-  const userData = useAppSelector((state) => state.user);
+  const { summoner_name, summoner_level, summoner_icon_url, rank, win_rate } = 
+    useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
 
   const handleClick = () => {
-    let prevLevel = userData.summoner_level || 0
-    dispatch(setUserData({ summoner_level: prevLevel + 1 }))
+    const prevLevel = summoner_level || 0;
+    dispatch(setUserData({ summoner_level: prevLevel + 1 }));
   }
 
   return (
     <header>
-
-      <Container maxWidth="xs" style={{ backgroundColor: 'skyblue'}}>
-        <WhiteTextTypography variant="h3">
-          Summoner Name: #Faker
-          Summoner level: 100
-        </WhiteTextTypography>
-        <h1>Rank: Challenger <br/>
-            Win Games: 10000<br/>
-            Loss Games: 9000
-        </h1>
+      <Container
+        maxWidth="xs"
+        sx={{ backgroundColor: 'skyblue' }}
+      >
+        <Avatar alt="summoner icon" src={summoner_icon_url} />
+        <Typography
+          variant="h5"
+          sx={{ color: 'white' }}
+        >
+          Summoner Name: {summoner_name}<br />
+          Summoner level: {summoner_level}<br />
+          Rank: {rank}<br/>
+          <br />
+          Win Rate: {win_rate}<br />
+          Win Games: 10000<br/>
+          Loss Games: 9000
+        </Typography>
       </Container>
-    </header>     
+    </header>
   );
 }
 
