c2fc0044f886ecbb62363b7f9ebf8f85cb68884f
Benson Zhou <ZHOUCHUNmason123@gmail.com>
2021-10-14 22:15:11 -0700

    Change the overview page with a container

--- frontend/src/pages/Overview.tsx
+++ frontend/src/pages/Overview.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 import { useAppSelector, useAppDispatch } from '../redux/hooks';
 import { setUserData } from '../redux/slices/user';
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const WhiteTextTypography = withStyles({
+  root: {
+    color: "#FFFFFF"
+  }
+})(Typography);
 
 function Overview() {
   const userData = useAppSelector((state) => state.user);
@@ -14,10 +25,21 @@ function Overview() {
 
   return (
     <header>
-      <button onClick={handleClick}>Increment level</button>
-      <div>Summoner Level: {userData.summoner_level}</div>
-    </header>
+
+      <Container maxWidth="xs" style={{ backgroundColor: 'skyblue'}}>
+        <WhiteTextTypography variant="h3">
+          Summoner Name: #Faker
+          Summoner level: 100
+        </WhiteTextTypography>
+        <h1>Rank: Challenger <br/>
+            Win Games: 10000<br/>
+            Loss Games: 9000
+        </h1>
+      </Container>
+    </header>     
   );
 }
+
+    
 
 export default Overview;
