ff0fcc4e07fbb6828dc491ed48e3ec99f8ddaceb
htran25 <htran25@ucsc.edu>
2021-10-21 21:17:20 -0700

    user-not-found message is a bit prettier now

--- frontend/src/pages/Overview.tsx
+++ frontend/src/pages/Overview.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Typography, Container, Avatar } from '@mui/material';
 
-import { useAppSelector, useAppDispatch } from '../redux/hooks';
-import { setUserData } from '../redux/slices/user';
+import { useAppSelector } from '../redux/hooks';
 
 const getProfileURL = (iconID?: number | null) => {
   return `http://ddragon.leagueoflegends.com/cdn/11.20.1/img/profileicon/${iconID}.png`;
@@ -22,27 +21,29 @@ function Overview() {
 
   return (
     <header>
-      {summonerFound ? (
         <Container
           maxWidth="xs"
           sx={{ backgroundColor: 'skyblue' }}
         >
-          <Avatar src={getProfileURL(summonerIcon)} />
-          <Typography
-            variant="h5"
-            sx={{ color: 'white' }}
-          >
-            Summoner Name: {summonerName}<br />
-            Summoner level: {summonerLevel}<br />
-            Rank: {rank}<br/>
-            <br />
-            Win Games: {winGames}<br/>
-            Loss Games: {lossGames}
-          </Typography>
+          {summonerFound ? (
+            <React.Fragment>
+              <Avatar src={getProfileURL(summonerIcon)} />
+              <Typography
+                variant="h5"
+                sx={{ color: 'white' }}
+              >
+                Summoner Name: {summonerName}<br />
+                Summoner level: {summonerLevel}<br />
+                Rank: {rank}<br/>
+                <br />
+                Win Games: {winGames}<br/>
+                Loss Games: {lossGames}
+              </Typography>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>User {summonerName} not found in {region}</React.Fragment>
+          )}
         </Container>
-      ) : (
-        <div>User {summonerName} not found in {region}</div>
-      )}
     </header>
   );
 }
