32a2c8bbc120453ab96ed8c40208fc0ba56bdbb3
sunny <sunny1103889616@gmail.com>
2021-10-24 12:48:16 -0700

    initializing the formatting of match analysis area. #34

--- frontend/src/pages/Overview/components/MatchSummary.tsx
+++ frontend/src/pages/Overview/components/MatchSummary.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { styled } from '@mui/material/styles';
 
 export default function MatchSummary() {
+
+    const MatchSummaryWrapper = styled('div')(({ theme }) => ({
+        height: '800px',
+        width: '100%',
+        textAlign: 'center',
+      }));
+
     return (
-        <div>
+        <MatchSummaryWrapper>
             match summary
-        </div>
+        </MatchSummaryWrapper>
     )
 }
