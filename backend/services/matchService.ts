b4cac65e5d33922699bb44bdc213d5c3c774e766
erlyu <ericloyu1@gmail.com>
2021-10-25 15:07:28 -0700

    I've set it up such that it would return the 10 lastest match ids of an account via an array, where array[0] = earleist game, and array[9] = 10th game played. #26

--- backend/services/matchService.ts
+++ backend/services/matchService.ts
@@ -2,17 +2,14 @@ import { info } from 'console';
 import * as riotApis from '../config/riotApis'
 import MatchDto from '../interfaces/IMatch/IMatchDto'
 
-const region = 'NA'
-const myid = 'qWqZJtdh6o_UYm0qJah8app2iPXfwrqWAKfddn8ORkxaiBJ_YQax8L8k-4atejesTfftcrak4OcBOg'
+// const region = 'your_region'
+// const myid = 'your_puuid'
 
 export const getMatchListByPUUID = async(puuid: string, region: string): Promise<Array<MatchDto> | undefined> => {
     try{
         const matchListInfo: Array<MatchDto> = await riotApis.findMatchHistoryInfo(puuid, region);
-        console.log(matchListInfo);
         return matchListInfo;
     } catch (error) {
         console.log(error);
     }
 }
-
-getMatchListByPUUID(myid, region);
