54020c6ea3f103f62f92832ca91c61a3c87a6a0c
erlyu <ericloyu1@gmail.com>
2021-10-25 18:20:06 -0700

    got match metadata and info elements printed out. #26

--- backend/services/matchService.ts
+++ backend/services/matchService.ts
@@ -5,11 +5,35 @@ import MatchDto from '../interfaces/IMatch/IMatchDto'
 // const region = 'your_region'
 // const myid = 'your_puuid'
 
-export const getMatchListByPUUID = async(puuid: string, region: string): Promise<Array<MatchDto> | undefined> => {
+const region = 'NA'
+const myid = 'qWqZJtdh6o_UYm0qJah8app2iPXfwrqWAKfddn8ORkxaiBJ_YQax8L8k-4atejesTfftcrak4OcBOg'
+const matchId = 'NA1_4080341688'
+
+//given puuid, return the last 10 match list id.
+const getMatchListByPUUID = async(puuid: string, region: string): Promise<Array<string>> => {
+    const matchListInfo: Array<string> = await riotApis.findMatchHistoryInfo(puuid, region);
+    if(matchListInfo.length >= 1){
+        // console.log(matchListInfo);
+        return matchListInfo as Array<string>;;
+    } else{
+        return [] as Array<string>;
+    }
+};
+
+//given match id, return the metadata and info element.
+export const getMatchInfoByMatchId = async(matchId: string, region: string): Promise<MatchDto | undefined> => {
     try{
-        const matchListInfo: Array<MatchDto> = await riotApis.findMatchHistoryInfo(puuid, region);
-        return matchListInfo;
+        const matchInfo: Array<MatchDto> = await riotApis.findMatchInfo(matchId, region);
+        console.log(matchInfo);
+        return;
     } catch (error) {
         console.log(error);
     }
-}
+};
+
+const main = async() =>{
+    const temp: Array<string> = await getMatchListByPUUID(myid, region);
+    getMatchInfoByMatchId(temp[0], region);
+};
+
+main()
