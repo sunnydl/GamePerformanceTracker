c4d9b1db7a0564628ae4532c7d770621e30a125e
Benson Zhou <ZHOUCHUNmason123@gmail.com>
2021-10-31 21:13:55 -0700

    add analysis of win lose function

--- backend/services/matchService.ts
+++ backend/services/matchService.ts
@@ -2,7 +2,7 @@ import { info } from 'console';
 import * as riotApis from '../config/riotApis'
 import MatchDto from '../interfaces/IMatch/IMatchDto'
 import ParticipantDto from '../interfaces/IMatch/IParticipantDto';
-
+import currency from 'currency.js';
 const myid = "qWqZJtdh6o_UYm0qJah8app2iPXfwrqWAKfddn8ORkxaiBJ_YQax8L8k-4atejesTfftcrak4OcBOg"
 const region = `NA`
 
@@ -37,7 +37,7 @@ const getMatchObjByMatchId = async(matchId: string, region: string): Promise<Mat
     return matchObj as MatchDto;
 }
 
-export const getMatchObjListByMatchList = async (match_list: Array<string>, region: string): Promise<Array<MatchDto> | undefined> => {
+export const getMatchObjListByMatchList = async (match_list: Array<string>, region: string): Promise<Array<MatchDto>> => {
     const matchDTOArr: Array<MatchDto> = [];
     for( const matchElem of match_list){
         const match = await getMatchObjByMatchId(matchElem, region);
@@ -47,13 +47,37 @@ export const getMatchObjListByMatchList = async (match_list: Array<string>, regi
     return matchDTOArr;
 }
 
+export const analysisMatchWinLoss = (puuid: string, matchList: Array<MatchDto>): Array<number> => {
+    const winlose: Array<number> = [];
+    let win = 0;
+    let lose = 0;
+    let win_lose = 0;
+    for(let i = matchList.length-1; i >= 0; i --){
+        const partis: Array<ParticipantDto> = matchList[i].info.participants;
+        for(let j = 0; j < partis.length; j++){
+            if(partis[j].puuid === puuid){
+                if(partis[j].win){
+                    win += 1;
+                }else{
+                    lose += 1;
+                }
+                win_lose = currency(win).divide(currency(lose).add(win)).value;
+                winlose.push(win_lose);     
+            }     
+        }
+    }
+
+    return winlose;
+}
+
 // given participant info array, locate info about curr player, and return
-const main = async() =>{
+/*const main = async() =>{
     const temp: Array<string> = await getMatchListByPUUID(myid, region);
     // console.log(temp);
-    await getMatchObjByMatchId(temp[0], region)
-    await getMatchObjListByMatchList(temp, region);
+    await getMatchObjByMatchId(temp[0], region);
+    //const matchlist: Array<MatchDto> = await getMatchObjListByMatchList(temp, region);
     await getParticipantsInfoByMatchId(temp[0], region);
+    //console.log(analysisMatchWinLoss(myid,matchlist));
 };
+*/
 
-main() 
