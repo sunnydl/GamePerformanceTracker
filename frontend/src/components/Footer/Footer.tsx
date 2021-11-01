e75d7ae28560614768268aa58a34952b638f2e78
erlyu <ericloyu1@gmail.com>
2021-10-31 20:59:51 -0700

    rid of errors. #37

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import { Typography, Tabs, Tab, Avatar } from '@mui/material';
+import { Tabs, Tab, Avatar } from '@mui/material';
 import { FooterWrapper, ParticipantsWrapper } from './style';
 
 export default function Footer() {
