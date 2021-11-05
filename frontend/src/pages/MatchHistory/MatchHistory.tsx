34eed541a30a110029f06723120ef9d57f53f65c
htran25 <htran25@ucsc.edu>
2021-11-04 19:28:20 -0700

    match list setup. #57

--- frontend/src/pages/MatchHistory/MatchHistory.tsx
+++ frontend/src/pages/MatchHistory/MatchHistory.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+import { Avatar, List, ListItem, ListItemAvatar } from '@mui/material';
+
 export default function Overview() {
+    const matches: any[] = [1, 2, 3]; // TODO: get match data from endpoint
+
     return (
         <React.Fragment>
-            Placeholder
+            <List sx={{ width: '90%', margin: 'auto' }}>
+                {matches.map((match, idx) => (
+                    <ListItem key={idx} divider>
+                        <ListItemAvatar>
+                            <Avatar src={undefined/* Player's champion during that match */} sx={{ width: '128px', height: '128px' }} />
+                        </ListItemAvatar>
+                        {/* TODO: add other components */}
+                        {/* 
+                            <Grid container>
+                                <Grid item>
+                                    <MatchResults />
+                                </Grid>
+                                <Grid item>
+                                    <MatchStats />
+                                </Grid>
+                            </Grid>
+                            <MatchSummary className='absolute-position top-right'>
+                                <div>position</div>
+                                <div>lane</div>
+                                <div>gamemode</div>
+                                <div>date</div>
+                            </MatchInfo>
+                        */}
+                    </ListItem>
+                ))}
+            </List>
         </React.Fragment>
     )
 }
