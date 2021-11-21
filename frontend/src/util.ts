00b0ec77d0370ddd5a44219d243b4f64fbf6b014
htran25 <htran25@ucsc.edu>
2021-11-20 19:31:51 -0800

    add default values for calculateWinRate helper function

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -10,7 +10,7 @@ export const getChampionIconURL = (championName?: string) => {
     return `http://ddragon.leagueoflegends.com/cdn/11.22.1/img/champion/${championName}.png`;
 }
 
-export const calculateWinRate = (wins: number, losses: number, asPercent?: boolean) => {
+export const calculateWinRate = (wins: number=0, losses: number=0, asPercent?: boolean) => {
     const winRate = currency(wins).divide(wins + losses);
     return asPercent ? `${winRate.intValue || 0}%` : (winRate.value || 0).toFixed(2);
 }
