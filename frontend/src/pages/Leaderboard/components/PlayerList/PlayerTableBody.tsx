93fd20c498fa419b44765b42abdf7c9e1309bd08
htran25 <htran25@ucsc.edu>
2021-11-20 21:54:50 -0800

    added win loss ratio bar to leaderboard entries

--- frontend/src/pages/Leaderboard/components/PlayerList/PlayerTableBody.tsx
+++ frontend/src/pages/Leaderboard/components/PlayerList/PlayerTableBody.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useAppSelector } from '../../../../redux/hooks';
-import { Avatar, TableBody, TableRow, TableCell } from '@mui/material';
+import { useTheme, Avatar, Box, TableBody, TableRow, TableCell } from '@mui/material';
+import RatioBar from '../../../../components/RatioBar';
 
-import { getSummonerIconURL, calculateWinRate } from '../../../../util';
+import { getSummonerIconURL, displayWinRate } from '../../../../util';
 
 export default function PlayerTableBody() {
+    const theme = useTheme();
     const leaderboard = useAppSelector((state) => state.leaderboard.leaderboard);
 
     return (
@@ -12,11 +14,23 @@ export default function PlayerTableBody() {
             {leaderboard.map((player, idx) => (
                 <TableRow key={`${player.summonerName}#${player.region}`}>
                     <TableCell>{idx + 1}</TableCell>
-                    <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Avatar src={getSummonerIconURL(player.summonerIcon)} />
-                        {player.summonerName}
+                    <TableCell>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                            <Avatar src={getSummonerIconURL(player.summonerIcon)} />
+                            {player.summonerName}
+                        </Box>
                     </TableCell>
-                    <TableCell>{calculateWinRate(player.winGames, player.lossGames, true)}</TableCell>
+                    <TableCell>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                            <RatioBar
+                                title={displayWinRate(player.winGames, player.lossGames, true)}
+                                firstValue={player.winGames} 
+                                secondValue={player.lossGames}
+                                firstColor={theme.palette.info.light}
+                                secondColor={theme.palette.error.light}
+                            />
+                        </Box>
+                    </TableCell>
                     <TableCell>{player.summonerLevel}</TableCell>
                     <TableCell>{player.rank}</TableCell>
                     <TableCell>{player.leaguePoints}</TableCell>
--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -10,7 +10,12 @@ export const getChampionIconURL = (championName?: string) => {
     return `http://ddragon.leagueoflegends.com/cdn/11.22.1/img/champion/${championName}.png`;
 }
 
-export const calculateWinRate = (wins: number=0, losses: number=0, asPercent?: boolean) => {
-    const winRate = currency(wins).divide(wins + losses);
-    return asPercent ? `${winRate.intValue || 0}%` : (winRate.value || 0).toFixed(2);
+export const calculateWinRate = (wins: number=0, losses: number=0) => {
+    if (wins + losses === 0) return currency(0);
+    return currency(wins).divide(wins + losses);
+}
+
+export const displayWinRate = (wins: number=0, losses: number=0, asPercent?: boolean) => {
+    const winRate = calculateWinRate(wins, losses);
+    return asPercent ? `${winRate.intValue}%` : (winRate.value).toFixed(2);
 }
