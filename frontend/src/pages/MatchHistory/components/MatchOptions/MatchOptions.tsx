fc4bfdbad1959962551e73239bdcd7c635be6932
sunny <sunny1103889616@gmail.com>
2021-11-20 20:34:30 -0800

    added transition animation and implemented silders

--- frontend/src/pages/MatchHistory/components/MatchOptions/MatchOptions.tsx
+++ frontend/src/pages/MatchHistory/components/MatchOptions/MatchOptions.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
-import { Button, ButtonGroup } from '@mui/material';
+import { Slider, Box } from '@mui/material';
 
 function MatchOptions({ options, setOption }: { options: number[], setOption: React.Dispatch<React.SetStateAction<number>> }) {
+
+    const marks = options.map((option: number) => {
+        return {
+            value: option,
+            label: option===1? `${option} game`:`${option} games`,
+        }
+    })
+
+    const handleChange = (
+        event: React.SyntheticEvent | Event, value: any
+    ) => {
+        setOption(value);
+    }
+
     return (
-        <ButtonGroup data-testid='size-buttons'>
-            {options.map((choice, idx) => (
-                <Button key={idx} onClick={() => setOption(choice)}>
-                    {choice} Games
-                </Button>
-            ))}
-        </ButtonGroup>
+        <Box sx={{
+            width: '70%'
+        }}>
+            <Slider
+                aria-label="Number of games"
+                defaultValue={3}
+                getAriaValueText={(value: number) => `${value}`}
+                valueLabelDisplay="auto"
+                marks={marks}
+                max={10}
+                min={1}
+                onChangeCommitted={handleChange}
+            />
+        </Box>
     );
 }
 
