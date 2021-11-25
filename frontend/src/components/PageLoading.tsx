5ab096cf9866da3799199d6180d70c826152b452
htran25 <htran25@ucsc.edu>
2021-11-25 00:18:31 -0800

    improved loading animation

--- frontend/src/components/PageLoading.tsx
+++ frontend/src/components/PageLoading.tsx
@@ -3,7 +3,13 @@ import { CircularProgress, Box } from '@mui/material'
 
 export default function PageLoading() {
     return (
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{
+            display: 'flex',
+            position: 'fixed',
+            bottom: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}>
             <CircularProgress />
         </Box>
     )
