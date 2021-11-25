8ab6d74dded735b7eb3afdb6c0edc2603c680042
htran25 <htran25@ucsc.edu>
2021-11-25 00:33:45 -0800

    bigger loading icon

--- frontend/src/components/PageLoading.tsx
+++ frontend/src/components/PageLoading.tsx
@@ -10,7 +10,7 @@ export default function PageLoading() {
             left: '50%',
             transform: 'translate(-50%, -50%)'
         }}>
-            <CircularProgress />
+            <CircularProgress size={128} />
         </Box>
     )
 }
