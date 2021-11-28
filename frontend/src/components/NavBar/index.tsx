241724916eb92a82904f115d33c4087ecf52a7bd
Jack Vento <jackovento@gmail.com>
2021-11-28 12:51:53 -0800

    Navbar and background

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -2,13 +2,43 @@ import React from 'react';
 
 import { Link, useLocation } from 'react-router-dom';
 import { useAppSelector } from '../../redux/hooks';
-import { Box, Button, AppBar, Toolbar, IconButton, Tab } from '@mui/material';
+import { AppBar, Select, Toolbar, IconButton, Tab } from '@mui/material';
 import { NavBarTabs } from './style';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import SummonerSearchBar from './SummonerSearchBar';
 
 function NavBar() {
   const location = useLocation();
   const summonerFound = useAppSelector((state) => state.user.summonerFound);
+  const useDropdown = useMediaQuery(useTheme().breakpoints.up('lg'));
+
+  let selection;
+  if (useDropdown)
+  {
+    selection = 
+      <NavBarTabs value={false}>
+        <Tab label='overview' component={Link} to={`/overview${location.search}`} disabled={!summonerFound} />
+        <Tab label='Match History' component={Link} to={`/match-history${location.search}`} disabled={!summonerFound} />
+        <Tab label='Leaderboard' component={Link} to={`/leaderboard${location.search}`} />
+        <Tab label='Champion' component={Link} to={{ pathname: "https://www.leagueoflegends.com/en-us/champions/" }} target="_blank"/>
+      </NavBarTabs>;
+  } else {
+    selection = 
+      <Select
+        labelId="page-selector"
+        sx={{ flexGrow: 0.5 }}
+      >
+        <Tab label='overview' component={Link} to={`/overview${location.search}`} disabled={!summonerFound} />
+        <Tab label='Match History' component={Link} to={`/match-history${location.search}`} disabled={!summonerFound} />
+        <Tab label='Leaderboard' component={Link} to={`/leaderboard${location.search}`} />
+        <Tab label='Champion' component={Link} to={{ pathname: "https://www.leagueoflegends.com/en-us/champions/" }} target="_blank"/>
+      </Select>;
+  }
+
+  if (location.pathname === '/') {
+    return null;
+  }
 
   return (
       <AppBar position='sticky' color='secondary' elevation={0}> 
@@ -24,22 +54,8 @@ function NavBar() {
           >
             GPT {/* TODO: add icon */}
           </IconButton>
-          <Box sx={{ flexGrow: 0.05 }} />
-          <NavBarTabs value={false}>
-            <Tab label='overview' component={Link} to={`/overview${location.search}`} disabled={!summonerFound} />
-            <Tab label='Match History' component={Link} to={`/match-history${location.search}`} disabled={!summonerFound} />
-            <Tab label='Leaderboard' component={Link} to={`/leaderboard${location.search}`} />
-            <Tab label='Champion' component={Link} to={{ pathname: "https://www.leagueoflegends.com/en-us/champions/" }} target="_blank"/>
-          </NavBarTabs> 
+          {selection}
           <SummonerSearchBar />
-          <Box sx={{ flexGrow: 0.5 }} />
-          <Button color="secondary" variant="contained">
-            Log in
-          </Button>
-          <Box sx={{ flexGrow: 0.02 }} />
-          <Button color="primary" variant="contained">
-            Register
-          </Button>
         </Toolbar>
       </AppBar>
   );
